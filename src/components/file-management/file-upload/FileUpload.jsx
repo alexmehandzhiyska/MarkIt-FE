4093dfbf0020fd7d272f523fa6bed25e07561abf
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2024-03-30 14:40:00 +0200

    Added project creation functionality

--- src/components/file-management/file-upload/FileUpload.jsx
+++ src/components/file-management/file-upload/FileUpload.jsx
@@ -1,12 +1,12 @@
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { fileService } from "../../services/fileService";
+import { fileService } from "../../../services/fileService";
 
-import uploadIcon from "../../assets/upload-icon.svg";
-import styles from './FileUpload.module.css';
+import uploadIcon from "../../../assets/upload-icon.svg";
+import styles from '../Modal.module.css';
 import { Modal } from "@mui/material";
 
-const FileUpload = ({ addPopupShown, setAddPopupShown, setAdditionalBtnsShown }) => {
+const FileUpload = ({ filePopupShown, setFilePopupShown, setAdditionalBtnsShown }) => {
     const navigate = useNavigate();
     const fileRef = useRef(null);
 
@@ -16,13 +16,13 @@ const FileUpload = ({ addPopupShown, setAddPopupShown, setAdditionalBtnsShown })
 
         const formData = new FormData();
         formData.append('file', file);
-        setAddPopupShown(false);
+        setFilePopupShown(false);
         setAdditionalBtnsShown(false);
 
         fileService.uploadFile(formData)
             .then(res => {
                 console.log(res.analysis);
-                setAddPopupShown(false);
+                setFilePopupShown(false);
                 setAdditionalBtnsShown(false);
                 navigate('/');
             })
@@ -32,15 +32,15 @@ const FileUpload = ({ addPopupShown, setAddPopupShown, setAdditionalBtnsShown })
     }
 
     return (
-        <Modal open={addPopupShown}>
-            <section className={`${styles.fileUploadContainer} ${styles.modalView}`}>
+        <Modal open={filePopupShown}>
+            <section className={`${styles.uploadContainer} ${styles.modalView}`}>
                 <form onSubmit={submitFile} className={styles.alignElements}>
                     <img src={uploadIcon} alt="Upload folder icon" onClick={() => fileRef.current.click()} />
                     <input type="file" name="file" className={styles.fileInput} ref={fileRef} />
                     <h3>Drop your files here to upload them</h3>
 
                     <div className={styles.btnsContainer}>
-                        <button className={`${styles.btn} ${styles.btnSecondary}`} onClick={() => { setAddPopupShown(false); setAdditionalBtnsShown(false) }}>Cancel</button>
+                        <button className={`${styles.btn} ${styles.btnSecondary}`} onClick={() => { setFilePopupShown(false); setAdditionalBtnsShown(false) }}>Cancel</button>
                         <button type="submit" className={`${styles.btn} ${styles.btnPrimary}`}>Upload</button>
                     </div>
                 </form>
