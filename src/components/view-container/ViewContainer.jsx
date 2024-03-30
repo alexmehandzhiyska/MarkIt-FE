cdb1226c455e2b5681ed65498581753b793eb83e
Alexandrina Mehandzhiyska <alexandrina.mehandzhiyska1@gmail.com>
2024-03-30 18:56:35 +0200

    chore: split layout structure for new features

--- src/components/view-container/ViewContainer.jsx
+++ src/components/view-container/ViewContainer.jsx
@@ -1,11 +1,24 @@
 import { Grid } from "@mui/material";
 import React from "react";
 
+import Summaries from "./summaries/Summaries";
+import Chat from "./chat/Chat";
+
+import styles from "./ViewContainer.module.css";
+
 const ViewContainer = () => {
     return (
-        <Grid padding={"24px"}>
-            <p>*User Profiles: 1. Seasoned Investor 2. Small Business Owner 3. Financial Journalist 4. Stay-at-Home Parent 5. Financial Advisor at a Large Investment Firm 6. Recent College Graduate 7. Retired Accountant 8. Financial Analyst at a Large Hedge Fund 9. Financial Planner 10. Part-Time Investor 11. Financial Consultant 12. College Professor 13. Financial Advisor specializing in Sustainable and Impact Investing 14. Risk Manager at a Large Financial Institution 15. Financial Blogger Revenue: - WealthTrack revenue from user subscriptions: $X million Sales: - Number of active users on WealthTrack platform: X,XXX - Percentage of users considering exploring other financial management tools: XX% - Percentage of users hopeful for future updates to address platform challenges: XX% Market Trends: - Growing demand for sustainable investing solutions - Increasing need for advanced modeling and simulation capabilities in financial platforms Feedback: - Mixed feedback from users regarding platform customization and advanced features Platform Improvements: - Enhancing data availability and transparency around ESG metrics - Addressing limitations in retirement income planning and tax optimization features - Improving functionality and user experience for better customization options Competitor Analysis: - Evaluation of other platforms by financial analysts and risk managers for advanced modeling and risk management capabilities Education:** - WealthTrack's user-friendly interface and educational resources positively impacting college professors and students in finance courses.</p>
-        </Grid>
+        // Removed Box to apply padding directly in CSS, offering more control
+        <div className={styles.viewContainer}>
+            <Grid container spacing={3} style={{ width: 'auto', margin: 0 }}>
+                <Grid item xs={8}>
+                    <Summaries />
+                </Grid>
+                <Grid item xs={4}>
+                    <Chat />
+                </Grid>
+            </Grid>
+        </div>
     );
 };
 
