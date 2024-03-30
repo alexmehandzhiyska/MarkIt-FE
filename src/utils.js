828cbe7fa192fed1602d7a04caaad62d710a188a
Alexandrina Mehandzhiyska <alexandrina.mehandzhiyska1@gmail.com>
2024-03-30 20:55:56 +0200

    feat: dynamic sidebar

---
+++ src/utils.js
@@ -0,0 +1,63 @@
+const dataProjects = {
+    "hi": [
+        {
+            "file_path": "hi/myFolder/helloworld-3.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/helloworld-4.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/helloworld-5.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/helloworld-6.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/helloworld-7.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/helloworld-8.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/helloworld-9.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/helloworld-10.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/helloworld-11.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/helloworld-12.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/helloworld-13.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/helloworld-14.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/1.pdf",
+            "extension": "pdf"
+        },
+        {
+            "file_path": "hi/myFolder/helloworld-15.pdf",
+            "extension": "pdf"
+        }
+    ],
+    "hello test": [],
+    "asdfd": []
+};
+
