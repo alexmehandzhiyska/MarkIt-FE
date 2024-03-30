e88bb359f665935b74376a3fc6d9300fef553f3e
KonstantinNatev <76740873+KonstantinNatev@users.noreply.github.com>
2024-03-30 18:22:46 +0200

    fix: view container

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,37 +1,13 @@
 
 import userPicture from "../../../assets/profile-picture.svg";
-import menuIcon from "../../../assets/side-bar-menu.svg";
 import styles from "./Header.module.css";
-import SideBar from "../sidebar/Sidebar";
-import { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 
 import copyIcon from "../../../assets/copy-icon.svg";
 
 const Header = () => {
-    const [widthScreenSize, setWidthScreenSize] = useState(window.innerWidth); 
-    const [open, setOpen] = useState(false);
-
-    const toggleDrawer = (newOpen) => () => {
-        setOpen(newOpen);
-    };
-
-    useEffect(() => {
-        const handleResize = () => {
-            setWidthScreenSize(window.innerWidth);
-        };
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
     return (
-        <Grid className={styles.menusWrapper}>
-            {widthScreenSize <= 600 && (
-                <Grid position={"absolute"} right={"2%"} top={"3%"} onClick={() => setOpen(true)}>
-                    <img src={menuIcon} alt="menu-icon" />
-                </Grid>
-            )}
-            <SideBar widthScreenSize={widthScreenSize} open={open} toggleDrawer={toggleDrawer} />
+        <Grid display={"flex"}>
             <header>
                 <section className={styles.userProjectInfo}>
                     <div className={styles.userProfileImageWrapper}>
