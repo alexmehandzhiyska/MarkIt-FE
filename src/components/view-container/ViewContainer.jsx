20f701d9e4b3861b1383bde4b596835934844ba5
KonstantinNatev <76740873+KonstantinNatev@users.noreply.github.com>
2024-03-31 00:48:20 +0200

    feat: added chat design

--- src/components/view-container/ViewContainer.jsx
+++ src/components/view-container/ViewContainer.jsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Divider, Grid } from "@mui/material";
 import React from "react";
 
 import Summaries from "./summaries/Summaries";
@@ -8,13 +8,12 @@ import styles from "./ViewContainer.module.css";
 
 const ViewContainer = () => {
     return (
-        // Removed Box to apply padding directly in CSS, offering more control
         <div className={styles.viewContainer}>
-            <Grid container spacing={3} style={{ width: 'auto', margin: 0 }}>
+            <Grid container spacing={3} style={{ width: 'auto', margin: 0, height: "100%" }}>
                 <Grid item xs={8}>
                     <Summaries />
                 </Grid>
-                <Grid item xs={4}>
+                <Grid height={"100%"} item xs={4}>
                     <Chat />
                 </Grid>
             </Grid>
