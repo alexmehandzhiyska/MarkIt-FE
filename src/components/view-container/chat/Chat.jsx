aebfed7570efe80f216af0c35db092ca0f7b25b9
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2024-03-31 02:57:19 +0200

    feat: chat interface

--- src/components/view-container/chat/Chat.jsx
+++ src/components/view-container/chat/Chat.jsx
@@ -1,58 +1,69 @@
+import React, { useState, useRef } from "react";
 import { Stack, Grid, TextField } from "@mui/material";
-import React, { useState } from "react";
+
+import { chatService } from "../../../services/chatService";
+
 import styles from "./Chat.module.css";
 import sendIcon from "../../../assets/send-icon.svg"
 
 const Chat = () => {
-    const [messages, setMessages] = useState([
-        { text: "Hello!" },
-        { text: "Hi there!" },
-        { text: "How are you?" },
-        { text: "I'm doing well, thank you!" },
-        { text: "That's great to hear!" },
-        { text: "Can you help me with something?" },
-    ]);
+    const [messages, setMessages] = useState([]);
+    const [oldMessages, setOldMessages] = useState("");
     const [question, setQuestion] = useState("");
+
+    const questionInput = useRef();
+
+    const sendQuestion = () => {
+        questionInput.current.querySelector('input').value = "";
+        setMessages([...messages, question]);
+        chatService.sendPrompt(question, 'diyan', oldMessages)
+        .then(res => {
+                setMessages([...messages, question, res.result]);
+                setOldMessages([...oldMessages, res.result]);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
 
     return (
         <div className={styles.chatContainer}>
-            <div className={styles.chatPanel}>
-                <Grid className={styles.messageContainer} elevation={3}>
-                    {messages.map((message, index) => (
-                        <div
-                            key={index}
-                            className={`${styles.message} ${
-                                index % 2 === 0 ? styles.userMessage : ""
-                            }`}
-                        >
-                            <span 
-                                className={`${styles.message} ${
-                                    index % 2 === 0 ? styles.messageText : ""
-                                }`}>{message.text}</span>
-                        </div>
-                    ))}
-                </Grid>
-            </div>
-            <div className={styles.questionPanel}>
-                <Stack className={styles.questionInputDislikeContainer}>
-                    <div className={styles.textFieldContainer}>
-                                <TextField
-                                    className={styles.questionInputTextAreaDisLike}
-                                    sx={{
-                                    "& fieldset": { border: 'none' },
-                                    }}
-                                    placeholder="write something"
-                                />
-                                <div
-                                    className={styles.questionInputSendButton}
-                                    aria-label="Ask question button"
-                                >
-                                    <img src={sendIcon} className={styles.sendIcon} alt="" />
-                                </div>
-                            </div>
-                </Stack>
-            </div>
+        <div className={styles.chatPanel}>
+            <Grid className={styles.messageContainer} elevation={3}>
+                {messages.map((message, index) => (
+                    <div key={index} className={styles.messageDisplay}>
+                        {index % 2 === 0 && 
+                            <p className={styles.userQuestion}>{message}</p>
+                        }
+                        {index % 2 !== 0 &&
+                            <p className={styles.chatAnswer}>{message}</p>
+                        }
+                    </div>
+                ))}
+            </Grid>
         </div>
+        <div className={styles.questionPanel}>
+            <Stack className={styles.questionInputDislikeContainer}>
+                <div className={styles.textFieldContainer}>
+                            <TextField
+                                className={styles.questionInputTextAreaDisLike}
+                                sx={{
+                                "& fieldset": { border: 'none' },
+                                }}
+                                placeholder="Write something..."
+                                onChange={(e) => setQuestion(e.target.value)}
+                                ref={questionInput}
+                            />
+                            <div
+                                className={styles.questionInputSendButton}
+                                aria-label="Ask question button"
+                            >
+                                <img src={sendIcon} className={styles.sendIcon} alt="" onClick={sendQuestion} />
+                            </div>
+                        </div>
+            </Stack>
+        </div>
+    </div>
     );
 };
 
