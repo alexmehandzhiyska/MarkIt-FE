9d933cd4bdf5bef8692b8bfbe106585878ea40d5
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2024-03-31 05:18:01 +0300

    fix: removed unused logic

--- src/components/view-container/summaries/Summaries.jsx
+++ src/components/view-container/summaries/Summaries.jsx
@@ -7,12 +7,10 @@ import styles from "./Summaries.module.css";
 const Summaries = () => {
     const location = useLocation();
     const [analysis, setAnalysis] = useState("");
-    const [showFormatted, setShowFormatted] = useState(false);
 
     useEffect(() => {
         if (location.state && location.state.analysis) {
             setAnalysis(location.state.analysis);
-            setShowFormatted(false);
         } else {
             setAnalysis("");
         }
@@ -20,28 +18,8 @@ const Summaries = () => {
 
     return (
         <div className={styles.summaryWrapper}>
-            {!analysis && 
-                <div className={styles.noData}>
-                    <h2>There is no uploaded data</h2>
-                </div>
-            }
-            {!showFormatted ? (
-                <Typewriter
-                    onInit={(typewriter) => {
-                        typewriter.typeString(analysis)
-                            .callFunction(() => {
-                                setShowFormatted(true);
-                            })
-                            .start();
-                    }}
-                    options={{
-                        delay: 310,
-                        skipAddStyles: true,
-                    }}
-                />
-            ) : (
                 <ReactMarkdown>{analysis}</ReactMarkdown>
-            )}
+
         </div>
     );
 };
